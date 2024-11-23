6147495dd0a56ba03f9011f26203031e5f55fa4d
radhoda <rod.hoda@gmail.com>
2024-11-22 23:48:43 -0800

    Map successfully highlights country that user entered. Need to work on getting the country's name to show up

--- static/CountryGuessScript.js
+++ static/CountryGuessScript.js
@@ -1,21 +1,65 @@
-async function submitGuess(event) {
+const map = L.map('map').setView([20,0],2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '© OpenStreetMap contributors',
+}).addTo(map)
+
+let geojson;
+
+const defaultStyle = {
+    color: "#3388ff",
+    weight: 1,
+    fillOpacity: 0.5,
+};
+
+const highlightStyle = {
+    color: "#ff0000",
+    weight: 1,
+    fillOpacity: 0.5,
+};
+
+function onEachCountry(feature, layer) {
+    layer.on({
+        mouseover: () => layer.setStyle({ weight: 2, color: "#666"}),
+        mouseout: () => layer.setStyle(defaultStyle), 
+    });
+}
+
+if ('/static/data/countries.json') {
+    fetch('/static/data/countries.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            geojson = L.geoJSON(data, {
+                style: defaultStyle,
+
+                onEachFeature: onEachCountry,
+            }).addTo(map);
+        });
+    }
+else {
+    console.log("Did not find json file.")
+}
+
+function submitGuess(event) {
     event.preventDefault();
 
-    const countryInput = document.getElementById('country')
-    const country = countryInput.value;
+    const guessedCountry = document.getElementById('country').value;
 
-    const response = await fetch('/api/guess', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ country })
+    console.log(guessedCountry)
+
+    geojson.eachLayer(layer => {
+        const countryName = layer.feature.properties.NAME.toLowerCase();
+
+        if (countryName == guessedCountry) {
+            layer.setStyle(highlightStyle);
+            document.getElementById('result').innerText = 'Correct! you  guessed ${countryName}';
+            console.log(document.getElementById('result').innerText);        }
+        else {
+            layer.setStyle(defaultStyle);
+        }
     });
-
-    const data = await response.json();
-
-    document.getElementById('result').textContent = data.message;
-
-    countryInput.value = '';
-
 }
+
+console.log("End of CountryGuessScript.js")
